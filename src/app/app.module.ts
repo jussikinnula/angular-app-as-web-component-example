df9a39619a6e25cc19b4715c68cfdc48271e4d06
Jussi Kinnula <jussi.kinnula@efecte.com>
2019-03-28 08:43:11 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {
+  constructor(private injector: Injector) {
+    const myElement = createCustomElement(AppComponent, { injector });
+    customElements.define('angular-app-as-web-component-example', myElement);
+  }
+
+  ngDoBootstrap() {}
+}
